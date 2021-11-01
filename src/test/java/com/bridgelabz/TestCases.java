949417e7caa807d901470ad68a736a3d5a5c7d80
Siraj <sirajkhanc2305@gmail.com>
2021-11-01 12:08:41 +0530

    Added TestCases for 0-0 feet and 0 feet to 0 inch conversion

--- src/test/java/com/bridgelabz/TestCases.java
+++ src/test/java/com/bridgelabz/TestCases.java
@@ -15,4 +15,16 @@ public class TestCases {
         Length yard = new Length(Length.Unit.YARD, 1);
         Assert.assertEquals(yard, new Length(Length.Unit.FEET, 3));
     }
+
+    @Test
+    public void whenGiven2SameFeetValueObject_ifEqual_shouldReturnTrue() {
+        Length feet = new Length(Length.Unit.FEET, 0.0);
+        Assert.assertEquals(feet, new Length(Length.Unit.FEET, 0.0));
+    }
+
+    @Test
+    public void whenGivenFeetAndInchObjects_shouldNotEquals() {
+        Length feet = new Length(Length.Unit.FEET, 1.0);
+        Assert.assertNotEquals(feet, new Length(Length.Unit.INCH, 1.0));
+    }
 }
