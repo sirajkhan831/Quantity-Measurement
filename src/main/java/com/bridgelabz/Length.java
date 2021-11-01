4f570629b994e40d63524a6e41b588ddf53fdf84
Siraj <sirajkhanc2305@gmail.com>
2021-11-01 12:03:13 +0530

    Added value() method

--- src/main/java/com/bridgelabz/Length.java
+++ src/main/java/com/bridgelabz/Length.java
@@ -5,17 +5,42 @@ import java.util.HashMap;
 /**
  * @author -> Siraj Khan
  */
-public class Length {
-    /**
-     * This program converts all the length values to inch and vice versa
-     */
-    private final Unit unit;
+public class Length {    /**
+ * This program converts all the length values to inch and vice versa
+ */
+private final Unit unit;
     private final double number;
-
+    private final double convertedValue;
     enum Unit {FEET, YARD, INCH, CENTIMETER}
 
     public Length(Unit unit, double number) {
         this.unit = unit;
         this.number = number;
+        this.convertedValue = value();
+    }
+    /**
+     * @value -> 1Feet=12Inch 1Yard=36Inch 1Cm=0.4Inch
+     * This method is converting all the values to Inch
+     * @return -> returns converted value
+     */
+    public double value() {
+        HashMap<Unit, Double> conversionTable = new HashMap<>();
+        conversionTable.put(Unit.FEET, 12 * number);
+        conversionTable.put(Unit.YARD, 36 * number);
+        conversionTable.put(Unit.INCH, 1 * number);
+        conversionTable.put(Unit.CENTIMETER, 0.4 * number);
+        return conversionTable.get(unit);
+    }
+    /**
+     * Overriding the equals method to make sure only one object exists of same value
+     * @param o -> Object
+     * @return -> Returns boolean value by comparing objects
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Length length = (Length) o;
+        return Double.compare(length.convertedValue, convertedValue) == 0;
     }
 }
