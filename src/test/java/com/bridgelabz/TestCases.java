8d4a0697465c31a1dc5a9182e43d533557480526
Siraj <sirajkhanc2305@gmail.com>
2021-11-01 12:13:13 +0530

    Added 4 TestCases for yard-inch and yard-feet conversion

--- src/test/java/com/bridgelabz/TestCases.java
+++ src/test/java/com/bridgelabz/TestCases.java
@@ -62,4 +62,27 @@ public class TestCases {
         Length feet = new Length(Length.Unit.FEET, 3.0);
         Assert.assertEquals(feet, new Length(Length.Unit.YARD, 1.0));
     }
+    @Test
+    public void whenGiven1InchAnd1Yard_shouldReturnNotEquals() {
+        Length inch = new Length(Length.Unit.INCH, 1.0);
+        Assert.assertNotEquals(inch, new Length(Length.Unit.YARD, 1.0));
+    }
+
+    @Test
+    public void whenGiven1YardAnd36Inch_shouldReturnEquals() {
+        Length yard = new Length(Length.Unit.YARD, 1.0);
+        Assert.assertEquals(yard, new Length(Length.Unit.INCH, 36.0));
+    }
+
+    @Test
+    public void whenGiven36InchAnd1Yard_shouldReturnEquals() {
+        Length inch = new Length(Length.Unit.INCH, 36.0);
+        Assert.assertEquals(inch, new Length(Length.Unit.YARD, 1.0));
+    }
+
+    @Test
+    public void whenGiven1YardAnd3Feet_shouldReturnEquals() {
+        Length yard = new Length(Length.Unit.YARD, 1.0);
+        Assert.assertEquals(yard, new Length(Length.Unit.FEET, 3.0));
+    }
 }
