7c62fb8a33ee5c0af5179814f92670fa07d45052
Siraj <sirajkhanc2305@gmail.com>
2021-11-01 12:00:07 +0530

    Added parameters and constructor in Length Class

--- src/main/java/com/bridgelabz/Length.java
+++ src/main/java/com/bridgelabz/Length.java
@@ -6,5 +6,16 @@ import java.util.HashMap;
  * @author -> Siraj Khan
  */
 public class Length {
+    /**
+     * This program converts all the length values to inch and vice versa
+     */
+    private final Unit unit;
+    private final double number;
 
+    enum Unit {FEET, YARD, INCH, CENTIMETER}
+
+    public Length(Unit unit, double number) {
+        this.unit = unit;
+        this.number = number;
+    }
 }
